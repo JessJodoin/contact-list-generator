85f391ea57107078fe4c9ef056199b9e09e3cdb4
Jessica Jodoin <jodo0083@algonquinlive.com>
2020-03-10 09:39:01 -0400

    Add name outputs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 var listContacts = function (contacts) {
+  document.write('<h1>Planetary peeps</h1>');
 
+  contacts.forEach(function (item) {
+    document.wirte(`<h2>${item.name}</h2>`)
+  });
 };
 
 listContacts(peepe);
